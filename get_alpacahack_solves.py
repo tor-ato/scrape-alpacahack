57bfa64048fdc04863d94f5d946d6c10c2e51ca3
tor-ato <toratokitahara@gmail.com>
2025-05-19 22:59:47 +0900

    scrape-alpacahack

---
+++ get_alpacahack_solves.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_alpacahack_solves(user):
+    response = requests.get(f"https://alpacahack.com/users/{user}")
+    soup = BeautifulSoup(response.content, features="html.parser")
+    tbody = soup.find("tbody",class_="MuiTableBody-root")
+    print(f"{'CHALLENGE':10}{'SOLVES':10}")
+    for i in tbody.find_all("tr"):
+        data = i.find_all("td")
+        print(f"{data[0].find("a").text:10}{data[1].find("p").text:10}")
+
+def get_alpacahack_solves(user):
+    response = requests.get(f"https://alpacahack.com/users/{user}")
+    soup = BeautifulSoup(response.content, features="html.parser")
+    tbody = soup.find("tbody",class_="MuiTableBody-root")
+    print({user})
+    print(f"{'CHALLENGE':20}{'SOLVES':20}{'SOLVED AT':20}")
+    for i in tbody.find_all("tr"):
+        data = i.find_all("td")
+        challenge = data[0].find("a").text
+        solves = data[1].find("p").text
+        solve_at = data[2].find("p").text
+        print(f"{challenge:20}{solves:20}{solve_at:20}")
---
+++ main.py
@@ -0,0 +1,7 @@
+from get_alpacahack_solves import get_alpacahack_solves
+def main():
+    get_alpacahack_solves("tor-ato")
+
+
+if __name__ == "__main__":
+    main()
