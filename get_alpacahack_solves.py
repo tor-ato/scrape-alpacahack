a1897afc662660b9d3ad2c1dd1a86a157daf275e
tor-ato <toratokitahara@gmail.com>
2025-05-20 15:46:26 +0900

    add: column_width

--- get_alpacahack_solves.py
+++ get_alpacahack_solves.py
@@ -4,12 +4,13 @@ from bs4 import BeautifulSoup
 def get_alpacahack_solves(user):
     response = requests.get(f"https://alpacahack.com/users/{user}")
     soup = BeautifulSoup(response.content, features="html.parser")
-    tbody = soup.find("tbody",class_="MuiTableBody-root")
+    tbody = soup.find("tbody", class_="MuiTableBody-root")
+    column_width = 15
     print({user})
-    print(f"{'CHALLENGE':20}{'SOLVES':20}{'SOLVED AT':20}")
+    print(f"{'CHALLENGE':{column_width}}{'SOLVES':{column_width}}{'SOLVED AT':{column_width}}")
     for i in tbody.find_all("tr"):
         data = i.find_all("td")
         challenge = data[0].find("a").text
         solves = data[1].find("p").text
         solve_at = data[2].find("p").text
-        print(f"{challenge:20}{solves:20}{solve_at:20}")
+        print(f"{challenge:{column_width}}{solves:{column_width}}{solve_at:{column_width}}")
